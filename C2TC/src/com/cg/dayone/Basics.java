e651cf84b4968c7f5177658755c4cbd0ed957eef
ELCOT <ELCOT@Lenovo>
2023-07-27 12:43:04 +0530

    commit

---
+++ C2TC/src/com/cg/dayone/Basics.java
@@ -0,0 +1,17 @@
+package com.cg.dayone;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=50;
+		int y=20;
+		 
+		System.out.println("Addition: "+(x+y));
+		System.out.println("Subraction: "+(x-y));
+		System.out.println("Multiplication: "+(x*y));
+		System.out.println("Division: "+(x/y));
+
+	}
+
+}
